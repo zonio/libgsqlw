a4da50d524e2b31f5ccd674b1c9eb77d9181a968
Ondrej Jirman <ondrej.jirman@zonio.net>
2007-09-11 11:15:51 +0200

    document gsqlw API

--- gsqlw.h
+++ gsqlw.h
@@ -3,7 +3,7 @@
 
 #include <glib.h>
 
-/** @file gsqlwrapper Header
+/** @file gsqlw Glib based SQL DB C interface wrapper.
  */
 
 typedef struct _gs_conn gs_conn;
@@ -11,24 +11,139 @@ typedef struct _gs_query gs_query;
 
 /* functions */
 
+/** Create connection to the database.
+ *
+ * @param dsn DSN specifies connection info. DSN consists of two parts separated
+ * by a colon. First part specifies backend that should be used and second part
+ * backend specific "connection" setup. Currently there are two supported backends:
+ * @li sqlite:file_path
+ * @li pgsql:dbname=test host=localhost user=postgres password=pass
+ *
+ * @return gs_conn on success NULL on error.
+ */
 gs_conn* gs_connect(const char* dsn);
+
+/** Disconnect and free database connection.
+ *
+ * All queries associated with this connection must be freed before calling this
+ * function.
+ *
+ * @param conn DB connection object.
+ */
 void gs_disconnect(gs_conn* conn);
+
+/** Get string describing last error.
+ *
+ * If this method returns non-NULL value all other methods except for
+ * gs_rollback() are inhibited.
+ *
+ * @return Error string or NULL.
+ */
 const char* gs_get_error(gs_conn* conn);
 
+/** Begin transaction on the given connection.
+ *
+ * @param conn DB connection object.
+ *
+ * @return -1 on error, 0 on success.
+ */
 int gs_begin(gs_conn* conn);
+
+/** Commit transaction on the given connection.
+ *
+ * @param conn DB connection object.
+ *
+ * @return -1 on error, 0 on success.
+ */
 int gs_commit(gs_conn* conn);
+
+/** Rollback transaction on the given connection.
+ *
+ * @param conn DB connection object.
+ *
+ * @return -1 on error, 0 on success.
+ */
 int gs_rollback(gs_conn* conn);
 
+/** Execute simple SQL command without returning any results.
+ *
+ * @param conn DB connection object.
+ * @param sql_string SQL command. This may contain $N substitutions.
+ * @param fmt Format string that defines number and type of substitutions given
+ * as remaining parameters to exec.
+ * @li s - const char*
+ * @li i - int
+ * @li ?i - int is_null, int val
+ *
+ * @return -1 on error, 0 on success.
+ *
+ * Example:
+ * gs_exec("INSERT INTO t(v1, v2) VALUES($1, $2)", "is", 1, "tes't");
+ */
 int gs_exec(gs_conn* conn, const char* sql_string, const char* fmt, ...);
 
+/** Create new SQL query.
+ *
+ * @param conn DB connection object.
+ * @param sql_string SQL query string.
+ *
+ * @return NULL on error, gs_query object on success.
+ */
 gs_query* gs_query_new(gs_conn* conn, const char* sql_string);
+
+/** Free query object.
+ *
+ * @param query Query object.
+ */
 void gs_query_free(gs_query* query);
 
+/** Get next row from the query result set.
+ *
+ * @param query Query object.
+ * @param fmt Fromat string describing format of row data, remaining parameters
+ * are pointers to variables that will be used to store row data according to
+ * the format string.
+ * @li s - const char**  - valid untill next gs_query_get call.
+ * @li S - char**        - caller must free returned data using g_free
+ * @li i - int*
+ * @li ?i - int* is_null, int* val
+ *
+ * @return -1 on error, 0 on success, 1 if no more rows avaliable.
+ */
 int gs_query_get(gs_query* query, const char* fmt, ...);
+
+/** (Re-)execute prepared query using given set of substitution parameters.
+ *
+ * @param query Query object.
+ * @param fmt Format string that defines number and type of substitutions given
+ * as remaining parameters to exec.
+ * @li s - const char*
+ * @li i - int
+ * @li ?i - int is_null, int val
+ *
+ * @return -1 on error, 0 on success.
+ */
 int gs_query_put(gs_query* query, const char* fmt, ...);
 
+/** Return number of rows that given query returns.
+ *
+ * May be only called after successfull gs_query_put.
+ *
+ * @param query Query object.
+ *
+ * @return -1 on error, number of rows on success.
+ */
 int gs_query_get_rows(gs_query* query);
-int gs_query_get_num(gs_query* query); /* get first column from the first row */
-int gs_query_get_last_id(gs_query* query, const char* seq_name); /* get last insert id */
+
+/** Return last inserted row's ID.
+ *
+ * May be only called after successfull gs_query_put or gs_exec.
+ *
+ * @param query Query object.
+ * @param seq_name Postgresql backend requires sequence name.
+ *
+ * @return -1 on error, positive ID on success.
+ */
+int gs_query_get_last_id(gs_query* query, const char* seq_name);
 
 #endif
